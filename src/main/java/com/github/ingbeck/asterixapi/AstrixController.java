38ee4e027952704df105feac084c26ae5e7be3eb
ingobecker <mail@ingobecker.de>
2024-02-15 12:58:59 +0100

    add put

--- src/main/java/com/github/ingbeck/asterixapi/AstrixController.java
+++ src/main/java/com/github/ingbeck/asterixapi/AstrixController.java
@@ -30,7 +30,18 @@ public class AstrixController {
     @DeleteMapping("/{id}")
     public List<AsterixCharacter> deleteCharacterByID(@PathVariable String id){
         asterixCharacterRepo.delete(asterixCharacterRepo.findById(id)
-                .orElseThrow(() -> new NoSuchElementException()));
+                .orElseThrow(() -> new NoSuchElementException("Element with Id: " + id + "not present")));
         return asterixCharacterRepo.findAll();
     }
+
+    @PutMapping("/{id}")
+    public AsterixCharacter updateCharacter(@PathVariable String id, @RequestBody AsterixCharacter asterixCharacter){
+        AsterixCharacter existingCharacter = asterixCharacterRepo.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Element with Id: " + id + "not present"));
+        existingCharacter.setAge(asterixCharacter.getAge());
+        existingCharacter.setName(asterixCharacter.getName());
+        existingCharacter.setProfession(asterixCharacter.getProfession());
+        return asterixCharacterRepo.save(existingCharacter);
+    }
+
 }
