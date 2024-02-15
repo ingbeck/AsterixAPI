55eec8ce00ba2d8cdf914d3a1b34db20e10e2734
ingobecker <mail@ingobecker.de>
2024-02-15 10:51:24 +0100

    initial commit

---
+++ src/main/java/com/github/ingbeck/asterixapi/AsterixApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.ingbeck.asterixapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsterixApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsterixApiApplication.class, args);
+    }
+
+}
